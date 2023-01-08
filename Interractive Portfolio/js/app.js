fb7b989ddb50f5d20d76c889f0a4f79de9be781a
Titus Faulkner Tull <118220478+Empairim@users.noreply.github.com>
2023-01-07 21:52:25 -0500

    Add files via upload

---
+++ Interractive Portfolio/js/app.js
@@ -0,0 +1,8 @@
+// typing Animation
+
+var typed = new Typed('.typing', {
+    strings:[',','Web Designer', 'Web Developer', 'UI Designer' ],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop:true
+})
