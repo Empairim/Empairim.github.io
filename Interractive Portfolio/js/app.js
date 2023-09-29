4518563a2187a2f8190cc0f0a38e878700d66404
Empairim <titusfaulknertull@yahoo.com>
2023-09-29 14:44:31 -0400

    just changed Image

--- Interractive Portfolio/js/app.js
+++
@@ -1,8 +0,0 @@
-// typing Animation
-
-var typed = new Typed('.typing', {
-    strings:[',','Web Designer', 'Web Developer', 'UI Designer' ],
-    typeSpeed: 100,
-    BackSpeed: 60,
-    loop:true
-})
